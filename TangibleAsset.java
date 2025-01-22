667e7f12500e1030437938087d70b192a1567457
Naoaihara <naoyoshi.aihara.0724@gmail.com>
2025-01-22 17:56:26 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,12 @@
+public abstract class TangibleAsset{
+  private String name;
+  private String color;
+   public TrangibleAsset(String name,int price,String color){
+     this.name = name;
+     this price = price;
+     this.color = coror;
+   }
+  public String getName(){return this.name;}
+  public int getPrice(){return this.price;}
+  public String getColor(){return this.color;}
+}
